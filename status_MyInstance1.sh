f08d11fd0a734993adb02126544570267acbb1ee
JonyGudino21 <90057500+JonyGudino21@users.noreply.github.com>
2025-01-06 18:27:34 -0600

    Update status_MyInstance1.sh

--- status_MyInstance1.sh
+++ status_MyInstance1.sh
@@ -14,8 +14,6 @@ ID=$(date +%s%N)
 # Verificar el estado y estructurar la respuesta JSON
 if [ $EXIT_CODE -eq 0 ]; then
     STATUS=$(echo "$STATUS_OUTPUT" | grep -o '"status": "[^"]*"' | awk -F':' '{print $2}' | tr -d '"')
-    STATUS="{ \"status\": \"$STATUS_VALUE\" }"
-    #STATUS=$(echo "$STATUS_OUTPUT" | grep -o '"status":"[^"]*"' | awk -F':' '{print $2}' | tr -d '"')
 else
     STATUS="error"
 fi
